c63dfd47771c4630b1cd54c05843607fdbe46468
Celso Alexandre <celsoalexandre@live.com>
2021-01-30 13:48:16 -0300

    Criacao de rota para upload e download de arquivos

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const Helpers = use('Helpers')
+const File = use('App/Models/File')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
+/**
+ * Resourceful controller for interacting with files
+ */
+class FileController {
+  /**
+   * Display a single file.
+   * GET files/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async show ({ params, request, response }) {
+    const file = await File.findOrFail(params.id)
+
+    return response.download(Helpers.tmpPath(`uploads/${file.file}`))
+  }
+
+  /**
+   * Create/save a new file.
+   * POST files
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '2mb' })
+
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), {
+        name: fileName
+      })
+
+      if (!upload.moved()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'Erro no upload de arquivo' } })
+    }
+  }
+}
+
+module.exports = FileController
